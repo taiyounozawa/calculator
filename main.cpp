08a87b2b06276549450d8aa18fec503636dfb564
taiyounozawa <f080vgw@yamaguchi-u.ac.jp>
2025-05-22 16:55:38 +0900

    commit test

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    //std::string str = "";
+    std::cout << "入力：";
+    std::cin >> a;
+    std::cout << a << std::endl;
+    
+    return 0;
+}
